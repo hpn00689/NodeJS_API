27c6602f8e164bee82430f4a7c61072152b3f773
hpn00689 <phuocnguyen1062002@gmail.com>
2022-06-22 20:39:42 +0700

    Init Git, Library NodeJS

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const bodyParser= require("body-parser");
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+app.get('/', (req, res) => res.send('Hello from Homepage'));
+app.listen(PORT, () => console.log('Server running on port: http://localhost:${PORT}'))
