d86bdea08905b91ba9c6ad9604edba5274cf6f8c
hpn00689 <phuocnguyen1062002@gmail.com>
2022-06-22 21:48:01 +0700

    Create sample data

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser= require("body-parser");
+const userRoutes = require('./routes/user.js')
 
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
+app.use('/users', userRoutes);
 app.get('/', (req, res) => res.send('Hello from Homepage'));
 app.listen(PORT, () => console.log('Server running on port: http://localhost:${PORT}'))
--- routes/user.js
+++ routes/user.js
@@ -2,4 +2,25 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get(''); 
+const users = [
+    {
+        "firstname": "Hoang",
+        "lastname": "Nguyen",
+        "age": 23
+    }, 
+    {
+        "firstname": "Thuy",
+        "lastname": "Long",
+        "age": 23
+    }, 
+]
+
+router.get('/', (req, res) => {
+    console.log(users);
+    res.send(users);
+}); 
+
+// router.post('/', (req, res) => {
+
+// })
+module.exports = router;
